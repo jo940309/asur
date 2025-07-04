5951fe831b9f9b837514f9ca51a7e1bc67bfd1e6
Jo <jojojojoyang@gmail.com>
2025-07-04 15:38:40 +0800

    feat: complete projects

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -1,9 +1,43 @@
 <template>
   <div id="projects" class="section projects-section">
-    <a class="bigPROJECTS">PROJECTS</a>
-    <h4 class="titleText">{{ getTitleById('projects') }}</h4>
+    <div class="row justify-center">
+      <div class="col-12">
+        <a class="bigPROJECTS">PROJECTS</a>
+        <h4 class="titleText">{{ getTitleById('projects') }}</h4>
+      </div>
+      <div class="col-12 flex justify-center">
+        <q-carousel
+          v-model="slide"
+          arrows
+          animated
+          swipeable
+          infinite
+          autoplay
+          :interval="2000"
+          control-type="flat"
+          class="projectsCarousel"
+        >
+          <q-carousel-slide
+            v-for="(item, index) in projects"
+            :key="index"
+            :name="index"
+            class="projectsCarouselSlide"
+          >
+            <img :src="item.img" alt="slide image" class="projectsImg" />
+            <div class="projectsText">
+              <div class="projectsTextTitle">{{ item.title }}</div>
+              <div class="projectsTextDescription">{{ item.description }}</div>
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { getTitleById } from '../util/title';
+import { ref } from 'vue';
+import { projects } from '../config/project';
+
+const slide = ref(0);
 </script>
---
+++ src/config/project.ts
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: 'title1',
+    description: 'description1',
+    img: 'src/assets/test/img1.jpg',
+  },
+  {
+    title: 'title2',
+    description: 'description2',
+    img: 'src/assets/test/img2.jpg',
+  },
+  {
+    title: 'title3',
+    description: 'description3',
+    img: 'src/assets/test/img3.jpg',
+  },
+  {
+    title: 'title4',
+    description: 'description4',
+    img: 'src/assets/test/img4.jpg',
+  },
+  {
+    title: 'title5',
+    description: 'description5',
+    img: 'src/assets/test/img5.jpg',
+  },
+  {
+    title: 'title6',
+    description: 'description6',
+    img: 'src/assets/test/img6.jpg',
+  },
+];
